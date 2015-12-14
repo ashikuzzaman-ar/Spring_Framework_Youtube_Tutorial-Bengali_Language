a08e06268bedbc9bc740b6e1aaeeb5900ae77731
ashik <ashik@ashik>
2015-12-14 16:15:04 +0600

    Commit After youtube lecture 7

--- src/java/com/tutorial/controller/IndexController.java
+++ src/java/com/tutorial/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.tutorial.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -12,8 +13,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class IndexController {
     
+    @RequestMapping(value = "/{index}/{inedex2}", method = RequestMethod.GET)
+    protected String requrstMapping(Model model,
+            @PathVariable(value = "index")String username) {
+
+        try {
+
+            model.addAttribute("message", "Hello Mr. "+username+", Wecome to Spring Framework!!!!!");
+        } catch (Exception e) {
+
+            model.addAttribute("errorMessage", e.toString());
+        }
+
+        return "index";
+    }
+    
     @RequestMapping(value = "/index", method = RequestMethod.GET)
-    protected String requrstMapping(Model model) {
+    protected String requrstMapping2(Model model) {
 
         try {
 
